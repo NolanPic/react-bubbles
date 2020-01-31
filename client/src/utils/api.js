1f6daba5e08f1f909cfa37e21278c38c9e87233d
Nolan Picini <nolanpic@gmail.com>
2020-01-31 08:40:50 -0800

    Fix axiosWithAuth and display bubbles

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -5,8 +5,6 @@ export const apiUrl = 'http://localhost:5000/api';
 export const axiosWithAuth = () => {
     return axios.create({
         baseURL: apiUrl,
-        headers: {
-            authentication: localStorage.getItem('token')
-        }
+        headers: { authorization: localStorage.getItem("token") }
     });
 };
