40845ba4c95f0f7b346042eb7950f4f898173846
Nolan Picini <nolanpic@gmail.com>
2020-01-31 08:20:31 -0800

    Add axiosWithAuth

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 export const apiUrl = 'http://localhost:5000/api';
+
+export const axiosWithAuth = () => {
+    return axios.create({
+        baseURL: apiUrl,
+        headers: {
+            authentication: localStorage.getItem('token')
+        }
+    });
+};
